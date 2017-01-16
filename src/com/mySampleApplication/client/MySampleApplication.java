94e26b171e086e0d3ced2c7e96c313a7b1a2cf14
dominasasa <o.dominas@gmail.com>
2017-01-16 21:22:38 +0100

    MainView refactor
    Refactored declaration of UI elements to a separate class

--- src/com/mySampleApplication/client/MySampleApplication.java
+++ src/com/mySampleApplication/client/MySampleApplication.java
@@ -8,174 +8,25 @@ import com.google.gwt.user.client.DOM;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.event.dom.client.ClickEvent;
 
-/**
- * Entry point classes define <code>onModuleLoad()</code>
- */
 public class MySampleApplication implements EntryPoint {
 
-    /**
-     * This is the entry point method.
-     */
-    RadioButton btnYellow;
-    RadioButton btnRed;
-    RadioButton btnGreen;
-    RadioButton btnBlue;
-    Button go;
-    Button newGame;
-    FlowPanel colorBtnGroup;
-
-    Label nickname;
-    Label grabChalk;
-
-    Label p1Nick;
-    Label p1Ratio;
-    Label name;
-    Label proc;
-    Label ratio;
-    Label p2Nick;
-    Label p2Ratio;
-
-
-    TextBox nickName;
-    Grid welcomeBox;
-    Grid p1Grid;
-
-    public void welcomePopUpBox() {
-        btnYellow = new RadioButton("colorGroup");
-        btnYellow.setValue(true);
-        btnRed = new RadioButton("colorGroup");
-        btnGreen = new RadioButton("colorGroup");
-        btnBlue = new RadioButton("colorGroup");
-        go = new Button("GO!");
-        grabChalk = new Label("GRAB CHALK!");
-        nickname = new Label("CHOOSE YOUR NICKNAME:");
-
-        nickName = new TextBox();
-        welcomeBox = new Grid(3,2);
-
-        colorBtnGroup = new FlowPanel();
-        colorBtnGroup.add(btnYellow);
-        colorBtnGroup.add(btnRed);
-        colorBtnGroup.add(btnGreen);
-        colorBtnGroup.add(btnBlue);
-
-        go.setStyleName("button-std");
-        btnYellow.setStyleName("btn-small");
-        btnRed.setStyleName("btn-small");
-        btnGreen.setStyleName("btn-small");
-        btnBlue.setStyleName("btn-small");
-
-        welcomeBox.setStyleName("welcome-box");
-        grabChalk.setStyleName("text_welcome");
-        nickname.setStyleName("text_welcome");
-        nickName.setStyleName("text_welcome_input");
-
-        welcomeBox.setWidget(0, 0, nickname);
-        welcomeBox.setWidget(0, 1, nickName);
-        welcomeBox.setWidget(1, 0, grabChalk);
-        welcomeBox.setWidget(2, 0, colorBtnGroup);
-        welcomeBox.setWidget(2, 1, go);
-    }
-
-    public void NewGamePopUpBox(){
-        welcomeBox.clear();
-        nickName.setText("");
-        newGame = new Button("CREATE\nGAME");
-        nickname.setText("ENTER GAME ID TO JOIN:");
-        go.setText("JOIN\nGAME");
-
-        go.setStyleName("button-std");
-        newGame.setStyleName("button-std");
-        welcomeBox.resizeRows(2);
-        welcomeBox.setWidget(0, 0, nickname);
-        welcomeBox.setWidget(0, 1, nickName);
-        welcomeBox.setWidget(1, 0, newGame);
-        welcomeBox.setWidget(1, 1, go);
-
-    }
-
-    public void makeP1(){
-        p1Grid = new Grid(2,2);
-        p1Nick = new Label("B");
-        name = new Label("PLAYER NAME: ");
-        p1Ratio = new Label("0");
-        proc = new Label("%");
-        ratio = new Label("RATIO: ");
-
-        p1Nick.setText(nickName.getText().toUpperCase());
-
-        p1Grid.setWidget(0, 0, name);
-        p1Grid.setWidget(0, 1, p1Nick);
-        p1Grid.setWidget(1, 0, ratio);
-        p1Grid.setWidget(1, 1, p1Ratio);
-        //p1Grid.setWidget(1, 1, proc);
-
-    }
-
     public void onModuleLoad() {
+        StartScreen MainPage = new StartScreen();
+        MainPage.welcomePopUpBox();
 
-        welcomePopUpBox();
-        final Button button = new Button("Click me");
-        final Label label = new Label();
-
-        //final Button newGame = new Button("NEW GAME");
-        //final Button joinGame = new Button("JOIN GAME");
-
-
-//        final Label enterGameId = new Label("ENTER GAME ID TO JOIN");
-
-        //final TextBox gameId = new TextBox();
-
-
-        button.addClickHandler(event -> {
-            if (label.getText().equals("")) {
-                MySampleApplicationService.App.getInstance().getMessage("Hello, World!", new MyAsyncCallback(label));
-            } else {
-                label.setText("");
-            }
-        });
-
-        go.addClickHandler(event -> {
-            if(btnYellow.getValue().equals(true))
+        MainPage.getGo().addClickHandler((ClickEvent event) -> {
+            if(MainPage.getBtnYellow().getValue().equals(true))
                 Document.get().getElementById("tab_p1").getStyle().setBackgroundColor("#FFF43A");
-            if(btnRed.getValue().equals(true))
+            if(MainPage.getBtnRed().getValue().equals(true))
                 Document.get().getElementById("tab_p1").getStyle().setBackgroundColor("#9E63FF");
-            if(btnGreen.getValue().equals(true))
+            if(MainPage.getBtnGreen().getValue().equals(true))
                 Document.get().getElementById("tab_p1").getStyle().setBackgroundColor("#33E35A");
-            if(btnBlue.getValue().equals(true))
+            if(MainPage.getBtnBlue().getValue().equals(true))
                 Document.get().getElementById("tab_p1").getStyle().setBackgroundColor("#33E35A");
-            makeP1();
-            NewGamePopUpBox();
-            RootPanel.get("tab_p1_text").add(p1Grid);
+            MainPage.makeP1();
+            MainPage.NewGamePopUpBox();
+            RootPanel.get("tab_p1_text").add(MainPage.getP1Grid());
         });
-
-        // Assume that the host HTML has elements defined whose
-        // IDs are "slot1", "slot2".  In a real app, you probably would not want
-        // to hard-code IDs.  Instead, you could, for example, search for all
-        // elements with a particular CSS class and replace them with widgets.
-        //
-        RootPanel.get("slot2").add(welcomeBox);
-
-
-
-
-
-        //RootPanel.get("slot2").add(label);
-    }
-
-    private static class MyAsyncCallback implements AsyncCallback<String> {
-        private Label label;
-
-        public MyAsyncCallback(Label label) {
-            this.label = label;
-        }
-
-        public void onSuccess(String result) {
-            label.getElement().setInnerHTML(result);
-        }
-
-        public void onFailure(Throwable throwable) {
-            label.setText("Failed to receive answer from server!");
-        }
+        RootPanel.get("slot2").add(MainPage.getWelcomeBox());
     }
 }
---
+++ src/com/mySampleApplication/client/StartScreen.java
@@ -0,0 +1,257 @@
+package com.mySampleApplication.client;
+
+import com.google.gwt.user.client.ui.*;
+
+/**
+ * Created by odomi on 16.01.2017.
+ */
+public class StartScreen {
+
+    private RadioButton btnYellow;
+    private RadioButton btnRed;
+    private RadioButton btnGreen;
+    private RadioButton btnBlue;
+    private Button go;
+    private Button newGame;
+    private FlowPanel colorBtnGroup;
+
+    private Label nickname;
+    private Label grabChalk;
+
+    private Label p1Nick;
+    private Label p1Ratio;
+    private Label name;
+    private Label proc;
+    private Label ratio;
+    private Label p2Nick;
+    private Label p2Ratio;
+
+    private TextBox nickName;
+    private Grid welcomeBox;
+    private Grid p1Grid;
+
+    public void welcomePopUpBox() {
+        setBtnYellow(new RadioButton("colorGroup"));
+        getBtnYellow().setValue(true);
+        setBtnRed(new RadioButton("colorGroup"));
+        setBtnGreen(new RadioButton("colorGroup"));
+        setBtnBlue(new RadioButton("colorGroup"));
+        setGo(new Button("GO!"));
+        setGrabChalk(new Label("GRAB CHALK!"));
+        setNickname(new Label("CHOOSE YOUR NICKNAME:"));
+
+        setNickName(new TextBox());
+        setWelcomeBox(new Grid(3,2));
+
+        setColorBtnGroup(new FlowPanel());
+        getColorBtnGroup().add(getBtnYellow());
+        getColorBtnGroup().add(getBtnRed());
+        getColorBtnGroup().add(getBtnGreen());
+        getColorBtnGroup().add(getBtnBlue());
+
+        getGo().setStyleName("button-std");
+        getBtnYellow().setStyleName("btn-small");
+        getBtnRed().setStyleName("btn-small");
+        getBtnGreen().setStyleName("btn-small");
+        getBtnBlue().setStyleName("btn-small");
+
+        getWelcomeBox().setStyleName("welcome-box");
+        getGrabChalk().setStyleName("text_welcome");
+        getNickname().setStyleName("text_welcome");
+        getNickName().setStyleName("text_welcome_input");
+
+        getWelcomeBox().setWidget(0, 0, getNickname());
+        getWelcomeBox().setWidget(0, 1, getNickName());
+        getWelcomeBox().setWidget(1, 0, getGrabChalk());
+        getWelcomeBox().setWidget(2, 0, getColorBtnGroup());
+        getWelcomeBox().setWidget(2, 1, getGo());
+    }
+
+    public void NewGamePopUpBox(){
+        getWelcomeBox().clear();
+        getNickName().setText("");
+        setNewGame(new Button("CREATE\nGAME"));
+        getNickname().setText("ENTER GAME ID TO JOIN:");
+        getGo().setText("JOIN\nGAME");
+
+        getGo().setStyleName("button-std");
+        getNewGame().setStyleName("button-std");
+        getWelcomeBox().resizeRows(2);
+        getWelcomeBox().setWidget(0, 0, getNickname());
+        getWelcomeBox().setWidget(0, 1, getNickName());
+        getWelcomeBox().setWidget(1, 0, getNewGame());
+        getWelcomeBox().setWidget(1, 1, getGo());
+
+    }
+
+    public void makeP1(){
+        setP1Grid(new Grid(2,2));
+        setP1Nick(new Label("B"));
+        setName(new Label("PLAYER NAME: "));
+        setP1Ratio(new Label("0"));
+        setProc(new Label("%"));
+        setRatio(new Label("RATIO: "));
+
+        getP1Nick().setText(getNickName().getText().toUpperCase());
+
+        getP1Grid().setWidget(0, 0, getName());
+        getP1Grid().setWidget(0, 1, getP1Nick());
+        getP1Grid().setWidget(1, 0, getRatio());
+        getP1Grid().setWidget(1, 1, getP1Ratio());
+        //p1Grid.setWidget(1, 1, proc);
+
+    }
+
+    public TextBox getNickName() {
+        return nickName;
+    }
+
+    public void setNickName(TextBox nickName) {
+        this.nickName = nickName;
+    }
+
+    public RadioButton getBtnGreen() {
+        return btnGreen;
+    }
+
+    public void setBtnGreen(RadioButton btnGreen) {
+        this.btnGreen = btnGreen;
+    }
+
+    public RadioButton getBtnBlue() {
+        return btnBlue;
+    }
+
+    public void setBtnBlue(RadioButton btnBlue) {
+        this.btnBlue = btnBlue;
+    }
+
+    public Button getGo() {
+        return go;
+    }
+
+    public void setGo(Button go) {
+        this.go = go;
+    }
+
+    public Button getNewGame() {
+        return newGame;
+    }
+
+    public void setNewGame(Button newGame) {
+        this.newGame = newGame;
+    }
+
+    public FlowPanel getColorBtnGroup() {
+        return colorBtnGroup;
+    }
+
+    public void setColorBtnGroup(FlowPanel colorBtnGroup) {
+        this.colorBtnGroup = colorBtnGroup;
+    }
+
+    public Label getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(Label nickname) {
+        this.nickname = nickname;
+    }
+
+    public Label getGrabChalk() {
+        return grabChalk;
+    }
+
+    public void setGrabChalk(Label grabChalk) {
+        this.grabChalk = grabChalk;
+    }
+
+    public Label getP1Nick() {
+        return p1Nick;
+    }
+
+    public void setP1Nick(Label p1Nick) {
+        this.p1Nick = p1Nick;
+    }
+
+    public Label getP1Ratio() {
+        return p1Ratio;
+    }
+
+    public void setP1Ratio(Label p1Ratio) {
+        this.p1Ratio = p1Ratio;
+    }
+
+    public Label getName() {
+        return name;
+    }
+
+    public void setName(Label name) {
+        this.name = name;
+    }
+
+    public Label getProc() {
+        return proc;
+    }
+
+    public void setProc(Label proc) {
+        this.proc = proc;
+    }
+
+    public Label getRatio() {
+        return ratio;
+    }
+
+    public void setRatio(Label ratio) {
+        this.ratio = ratio;
+    }
+
+    public Label getP2Nick() {
+        return p2Nick;
+    }
+
+    public void setP2Nick(Label p2Nick) {
+        this.p2Nick = p2Nick;
+    }
+
+    public Label getP2Ratio() {
+        return p2Ratio;
+    }
+
+    public void setP2Ratio(Label p2Ratio) {
+        this.p2Ratio = p2Ratio;
+    }
+
+    public Grid getWelcomeBox() {
+        return welcomeBox;
+    }
+
+    public void setWelcomeBox(Grid welcomeBox) {
+        this.welcomeBox = welcomeBox;
+    }
+
+    public Grid getP1Grid() {
+        return p1Grid;
+    }
+
+    public void setP1Grid(Grid p1Grid) {
+        this.p1Grid = p1Grid;
+    }
+
+    public RadioButton getBtnYellow() {
+        return btnYellow;
+    }
+
+    public void setBtnYellow(RadioButton btnYellow) {
+        this.btnYellow = btnYellow;
+    }
+
+    public RadioButton getBtnRed() {
+        return btnRed;
+    }
+
+    public void setBtnRed(RadioButton btnRed) {
+        this.btnRed = btnRed;
+    }
+}
+
