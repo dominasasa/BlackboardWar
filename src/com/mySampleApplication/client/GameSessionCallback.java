4e9bb06757b4b954c204bd31988294bd0ae28168
dominasasa <o.dominas@gmail.com>
2017-01-24 22:17:44 +0100

    GameSessionCallback.java documentation

--- src/com/mySampleApplication/client/GameSessionCallback.java
+++ src/com/mySampleApplication/client/GameSessionCallback.java
@@ -4,20 +4,33 @@ import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 
 /**
- * Created by abcd on 2017-01-22.
+ * Class implementing an interface AsyncCallback<T> to receive a response from RPC.
  */
 public class GameSessionCallback implements AsyncCallback<Player> {
 
+    /**
+     * Main window of the application.
+     */
     StartScreen startScreen;
 
+    /**
+     * Called when asynchronous call fails to complete normally.
+     * @param caught
+     */
     @Override
     public void onFailure(Throwable caught) {
         Window.alert("Can't create session!");
     }
 
+    /**
+     * Called when asynchronous call completes successfully.
+     * @param result
+     */
     @Override
     public void onSuccess(Player result) {
-        // Spawn board if session name is available
+        /**
+         * Spawn board if session name is available.
+         */
         if(result != null) {
             startScreen.spawnBoard(startScreen.getSessionID().getText(), result);
         } else {
